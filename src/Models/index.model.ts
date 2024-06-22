20179392ca3d807915c00bf1ab718e1c9d883974
David Chan <66018863+Duc-Developer@users.noreply.github.com>
2024-06-22 17:51:47 +0700

    Feature/mention blot (#15)
    * feat: update readme
    
    * feat: init mention module

--- src/Models/index.model.ts
+++ src/Models/index.model.ts
@@ -1,10 +1,12 @@
+import { CustomModuleKey, MentionModuleOptions } from '../Modules';
 import Quill, { EmitterSource } from 'quill';
-import { Delta, QuillOptions } from 'quill/core';
+import { Delta, QuillOptions, Module } from 'quill/core';
 
 export type RFQValue = string | Delta;
 export type RFQEventChange = (event: keyof typeof Quill.events, ...args: unknown[]) => void;
 export type RFQOnChange = (html: string, delta: Delta, oldContent: Delta, source: EmitterSource) => void;
-export interface IReactForQuill {
+export type RFQModules = Record<string, unknown> & Partial<Record<CustomModuleKey, MentionModuleOptions>>;
+export interface IReactForQuill extends QuillOptions {
   theme: string;
   readOnly?: boolean;
   value?: RFQValue;
@@ -12,9 +14,9 @@ export interface IReactForQuill {
   onChange?: RFQOnChange;
   onBlur?: (e: FocusEvent) => void;
   onDoubleClick?: (delta: Delta) => void;
-  options?: QuillOptions;
   onKeyUp?: (e: KeyboardEvent) => void;
   placeholder?: string;
   className?: string;
   onQuillEventChange?: RFQEventChange;
+  modules?: RFQModules;
 }
