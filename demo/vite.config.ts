152daf609b0b8195316ebca887fe69de7d8281bc
David Chan <66018863+Duc-Developer@users.noreply.github.com>
2024-06-24 08:50:43 +0700

    fix: change demo package.json (#40)
    Co-authored-by: unknown <ductt2@vmogroup.com>

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/react-for-quill/',
-  plugins: [react()],
+export default defineConfig(() =>  {
+  return {
+    base: '/react-for-quill/',
+    plugins: [react()],
+    server: {
+      host: process.env.HOST,
+      port: +(process.env.PORT || 3000),
+      open: true
+    },
+    preview: {
+      host: process.env.HOST,
+      port: 30001
+    },
+  }
 })
