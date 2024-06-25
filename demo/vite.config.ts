cc829f7081be4abf49e65d67aa956a958ad1f56f
David Chan <66018863+Duc-Developer@users.noreply.github.com>
2024-06-26 02:34:43 +0700

    Feature/enviroment (#45)
    * ci: define env
    
    * build: attachment file when build between dev & prod

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,11 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, Plugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig(() =>  {
+const BASE_URL = '/react-for-quill/';
+function htmlAttachment(): Plugin {
   return {
-    base: '/react-for-quill/',
-    plugins: [react()],
+    name: 'attachment-cdn-link',
+    transformIndexHtml(html) {
+      const isProduction = process.env.NODE_ENV === 'production';
+      const localPath = '/react-for-quill/node_modules/react-for-quill/dist/quill.snow.css';
+      const prodHref = 'https://cdn.jsdelivr.net/npm/react-for-quill@latest/dist/quill.snow.css';
+      const content = html.replace(
+        `${BASE_URL}__RFQ_SNOW_STYLE_LINK_ENV__`,
+        isProduction ? prodHref : localPath
+      );
+      return content;
+    },
+  };
+}
+
+function adjustModuleResolutionForDev(): Plugin {
+  return {
+    name: 'adjust-module-resolution-for-dev',
+    config(config, { command }) {
+      if (command === 'serve') {
+        config.resolve = config.resolve || {};
+        const alias = config.resolve.alias as Record<string, string>;
+        if (alias) {
+          const localPath = path.resolve(__dirname, './node_modules/react-for-quill/dist/index.esm.js');
+          alias['react-for-quill'] = localPath;
+        }
+      }
+    },
+  };
+}
+
+export default defineConfig(() => {
+  return {
+    base: BASE_URL,
+    plugins: [react(), htmlAttachment(), adjustModuleResolutionForDev()],
     server: {
       host: process.env.HOST,
       port: +(process.env.PORT || 3000),
@@ -15,5 +48,8 @@ export default defineConfig(() =>  {
       host: process.env.HOST,
       port: 30001
     },
+    resolve: {
+      alias: {}
+    }
   }
 })
